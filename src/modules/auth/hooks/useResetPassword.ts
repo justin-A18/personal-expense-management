9107e07a94b573ff1a2461a8692b651807ce7298
Justin Huertas <justin.hv08@gmail.com>
2025-09-29 11:58:21 -0500

    :recycle: replace next/router with next/navigation for routing

--- src/modules/auth/hooks/useResetPassword.ts
+++ src/modules/auth/hooks/useResetPassword.ts
@@ -1,5 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { ResetPasswordSchema, resetPasswordSchema } from '../schemas/auth.schema';
 
